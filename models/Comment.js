fd1543ca8ae6c1b56b7fd6d90fd5e0c218823def
Ethan Dutcher <74162204+edutcher@users.noreply.github.com>
2021-03-21 15:35:33 -0400

    models and server

--- models/Comment.js
+++ models/Comment.js
@@ -1,9 +1,9 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Post extends Model {}
+class Comment extends Model {}
 
-Post.init({
+Comment.init({
     id: {
         type: DataTypes.INTEGER,
         allowNull: false,
@@ -20,13 +20,20 @@ Post.init({
             model: 'user',
             key: 'id'
         }
+    },
+    post_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'post',
+            key: 'id'
+        }
     }
 }, {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'post',
+    modelName: 'comment',
 });
 
-module.exports = Post;
+module.exports = Comment;
